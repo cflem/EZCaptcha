b5d02d21438011d878907992c34ae748e7868513
Carson Fleming <carson.fl3ming@gmail.com>
2015-12-01 19:53:18 -1000

    Added example config.php

---
+++ config.php
@@ -0,0 +1,26 @@
+<?php
+
+### EZCaptcha Configuration ###
+
+## Cookies ##
+$c_name = "captcha";
+$c_expire = 0;
+$c_path = "/";
+$c_domain = ".cfl3ming.com";
+
+## CATPCHA String ##
+$s_len = 6;
+
+## Styling ##
+// background color: array(red, green, blue)
+$bg_color = array(200, 200, 200);
+// text rotation (degrees)
+$rotation_min = 0;
+$rotation_max = 90;
+// text color
+$t_color_min = array(0, 0, 0);
+$t_color_max = array(255, 255, 255);
+
+## Fonts ##
+$fonts_dir = "fonts/";
+$fonts = array("ubuntu", "arial", "verdana");
